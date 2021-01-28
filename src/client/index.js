7a57941469637b68a4c53e516f3c929c5508ad97
Felipe Stroff <stroff.felipe@gmail.com>
2021-01-28 14:06:45 -0300

    Implements async queryLayer

--- src/client/index.js
+++ src/client/index.js
@@ -10,130 +10,103 @@ require([
     esriRequest
 ) {
     form.addEventListener('submit', function (e) {
-        e.preventDefault();
-
         if (format.value === 'csv' ||
             format.value === 'xls') {
             convertSheet();
         }
         else if (format.value === 'geojson') {
-           convertGeojson(); 
+            convertGeojson(); 
         }
         else {
             extractData();
         }
+
+        e.preventDefault();
     });
 
-    function convertSheet() {
+    async function convertSheet() {
         btn.classList.add('esri-button--disabled');
         btn.innerText = 'Loading...';
 
         errMessage.style.display = 'none';
 
-        const queryTask = new QueryTask({
-            url: serviceUrl.value
+        const response = await queryLayer(false);
+        console.log('Query results', response);
+
+        var dataFormat, separator;
+        if (format.value === 'csv') {
+            dataFormat = 'data:text/csv';
+            separator = ',';
+        }
+        else {
+            dataFormat = 'data:application/vnd.ms-excel';
+            separator = ';';
+        }
+
+        var sheetContent = '';
+
+        response.fields.forEach(function (field) {
+            sheetContent += field.alias + separator;
         });
-    
-        const query = new Query();
-        query.returnGeometry = false;
-        query.outFields = ['*'];
-        query.where = '1=1';
-    
-        queryTask.execute(query).then(function(queryResults) {
-            console.log('Query results', queryResults);
 
-            var dataFormat, separator;
-            
-            if (format.value === 'csv') {
-                dataFormat = 'data:text/csv';
-                separator = ',';
-            }
-            else {
-                dataFormat = 'data:application/vnd.ms-excel';
-                separator = ';';
-            }
+        response.features.forEach(function (feature) {
+            sheetContent += '\r\n';
 
-            var sheetContent = '';
-
-            queryResults.fields.forEach(function (field) {
-                sheetContent += field.alias + separator;
+            Object.values(feature.attributes).forEach(function (attr) {
+                sheetContent += attr + separator;
             });
-
-            queryResults.features.forEach(function (feature) {
-                sheetContent += '\r\n';
-
-                Object.values(feature.attributes).forEach(function (attr) {
-                    sheetContent += attr + separator;
-                });
-            });
-    
-            sheetContent = `${dataFormat};charset=utf-8,%EF%BB%BF ${encodeURIComponent(sheetContent)}`;
-
-            const id = s4();
-
-            const ext = format.options[format.selectedIndex].dataset.ext;
-    
-            files.innerHTML += 
-            `<div class="esri-print__exported-file"
-                style="border-bottom: 1px solid #f3f3f3; padding-top: 5px; padding-bottom: 5px;">
-                <a download="data_${id}.${ext}" rel="noreferrer" target="_self" class="esri-widget__anchor esri-print__exported-file-link" href="${sheetContent}">
-                    <span class="esri-icon-download"></span><span class="esri-print__exported-file-link-title">data_${id}.${ext}</span>
-                </a>
-            </div>`;
-
-            files.style.display = 'block';
-
-            btn.classList.remove('esri-button--disabled');
-            btn.innerText = 'Convert';
-        })
-        .catch(function (e) {
-            handleException(e);
         });
+
+        sheetContent = `${dataFormat};charset=utf-8,%EF%BB%BF ${encodeURIComponent(sheetContent)}`;
+
+        const id = s4();
+
+        const ext = format.options[format.selectedIndex].dataset.ext;
+
+        files.innerHTML += 
+        `<div class="esri-print__exported-file"
+            style="border-bottom: 1px solid #f3f3f3; padding-top: 5px; padding-bottom: 5px;">
+            <a download="data_${id}.${ext}" rel="noreferrer" target="_self" class="esri-widget__anchor esri-print__exported-file-link" href="${sheetContent}">
+                <span class="esri-icon-download"></span><span class="esri-print__exported-file-link-title">data_${id}.${ext}</span>
+            </a>
+        </div>`;
+
+        files.style.display = 'block';
+
+        btn.classList.remove('esri-button--disabled');
+        btn.innerText = 'Convert';
     }
 
-    function convertGeojson() {
+    async function convertGeojson() {
         btn.classList.add('esri-button--disabled');
         btn.innerText = 'Loading...';
 
         errMessage.style.display = 'none';
 
-        const queryTask = new QueryTask({
-            url: serviceUrl.value
-        });
-    
-        const query = new Query();
-        query.returnGeometry = true;
-        query.outFields = ['*'];
-        query.where = '1=1';
-    
-        queryTask.execute(query).then(function(queryResults) {
-            console.log('Query results', queryResults);
+        const response = await queryLayer(true);
+        console.log('Query results', response);
 
-            const featureCollection = {
-                type: 'FeatureCollection',
-                features: queryResults.features.map(f => Terraformer.ArcGIS.parse(f))
-            };
-            
-            const geojsonContent = `data:application/geo+json;charset=utf-8,%EF%BB%BF ${encodeURIComponent(JSON.stringify(featureCollection))}`;
-            const id = s4();
-            const ext = format.options[format.selectedIndex].dataset.ext;
+        const featureCollection = {
+            type: 'FeatureCollection',
+            features: response.features.map(f => Terraformer.ArcGIS.parse(f))
+        };
+        
+        const geojsonContent = `data:application/geo+json;charset=utf-8,%EF%BB%BF ${encodeURIComponent(JSON.stringify(featureCollection))}`;
+        const id = s4();
+        const ext = format.options[format.selectedIndex].dataset.ext;
 
-            files.innerHTML += 
-            `<div class="esri-print__exported-file"
-                style="border-bottom: 1px solid #f3f3f3; padding-top: 5px; padding-bottom: 5px;">
-                <a download="data_${id}.${ext}" rel="noreferrer" target="_self" class="esri-widget__anchor esri-print__exported-file-link" href="${geojsonContent}">
-                    <span class="esri-icon-download"></span><span class="esri-print__exported-file-link-title">data_${id}.${ext}</span>
-                </a>
-            </div>`;
+        files.innerHTML += 
+        `<div class="esri-print__exported-file"
+            style="border-bottom: 1px solid #f3f3f3; padding-top: 5px; padding-bottom: 5px;">
+            <a download="data_${id}.${ext}" rel="noreferrer" target="_self" class="esri-widget__anchor esri-print__exported-file-link" href="${geojsonContent}">
+                <span class="esri-icon-download"></span><span class="esri-print__exported-file-link-title">data_${id}.${ext}</span>
+            </a>
+        </div>`;
 
-            files.style.display = 'block';
+        files.style.display = 'block';
 
-            btn.classList.remove('esri-button--disabled');
-            btn.innerText = 'Convert';
-        })
-        .catch(function (e) {
-            handleException(e);
-        });
+        btn.classList.remove('esri-button--disabled');
+        btn.innerText = 'Convert';
     }
 
     function extractData() {
@@ -261,10 +234,24 @@ require([
         });
     }
 
-    let s4 = () => {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
+    function queryLayer(geom) {
+        return new Promise(function(resolve, reject) {
+            const queryTask = new QueryTask({
+                url: serviceUrl.value
+            });
+        
+            const query = new Query();
+            query.returnGeometry = geom;
+            query.outFields = ['*'];
+            query.where = '1=1';
+        
+            queryTask.execute(query).then(function(queryResults) {
+                resolve(queryResults);
+            })
+            .catch(function (e) {
+                handleException(e);
+            });
+        });
     }
 
     function handleException(error) {
@@ -283,5 +270,11 @@ require([
         }
 
         errMessage.style.display = 'block'; 
+    }
+
+    let s4 = () => {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
     }
 });
